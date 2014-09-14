fc1c4dcf59ab494af37402c92caeb1ab6746ea29
Totem <majianglin@totemtec.com>
2014-09-14 10:02:20 +0800

    SVN Install Update

--- SVN Server Install/svn-update.sh
+++ SVN Server Install/svn-update.sh
@@ -1,7 +1,7 @@
 export LANG=en_US.UTF-8
 SVN=/usr/bin/svn
 WEB=/data/www2/zgtcb
-$SVN update $WEB --username majianglin --password h-.c3aV2 --no-auth-cache
+$SVN update $WEB --username majianglin --password xxxxxxxx --no-auth-cache
 chown -R www:www $WEB
 
 exit
