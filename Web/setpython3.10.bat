1a0eb60b85806c006c44918ea60945107561c00c
Totem <majianglin@totemtec.com>
2021-11-04 16:48:19 +0800

    Error Handler

--- Web/setpython3.10.bat
+++ Web/setpython3.10.bat
@@ -1,11 +1,17 @@
 @echo OFF
 
+chcp 65001
+
 set PYTHON_HOME=C:\Programs\Python\Python310
 
 set PATH=%PYTHON_HOME%;%PYTHON_HOME%\Scripts\;%PATH%
 
 setx PYTHON_HOME "%PYTHON_HOME%;%PYTHON_HOME%\Scripts\;" /m
 
-python --version
+IF %ERRORLEVEL% NEQ 0 ( 
+   echo "请以管理员身份来运行" 
+) ELSE (
+   python --version
+)
 
 pause
