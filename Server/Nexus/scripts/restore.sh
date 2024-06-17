6a57208a9222545a977d8137cf1cbab0f9631528
马江林 <majianglin@totemtec.com>
2024-06-17 14:31:38 +0800

    Nexus 备份调整
    db目录不再备份，而是做单独的清理

--- Server/Nexus/scripts/restore.sh
+++ Server/Nexus/scripts/restore.sh
@@ -12,7 +12,7 @@ echo "nexus killed"
 
 # 将备份文件恢复
 
-docker exec -it -u root nexus /bin/bash -c "cd /nexus-data && rm -rf keystores blobs restore-from-backup/* cache/* db/component db/config db/security && tar -xzf archive.tar.gz && mv -f BackUp/* restore-from-backup/ && exit"
+docker exec -it -u root nexus /bin/bash -c "cd /nexus-data && rm -rf keystores/node blobs restore-from-backup/* cache/* db/component db/config db/security && tar -xzf archive.tar.gz && mv -f BackUp/* restore-from-backup/ && exit"
 
 echo "restore files write finished"
 
@@ -26,6 +26,8 @@ echo "restart nexus"
 
 sleep 3
 
+echo "you can using nexus service 2 minites later."
+
 # 5分钟后清除备份文件
 
 docker exec -d -u root nexus /bin/bash -c "sleep 300; rm -rf BackUp archive.tar.gz; rm -rf /nexus-data/restore-from-backup/*.bak"
@@ -34,4 +36,4 @@ echo "clean will execute after 5 minutes"
 
 sleep 3
 
-echo "DONE. you can using nexus service 5 minites later."
+echo "Finish."
